59d49eee3010160df83f24312a725594acc9edc0
LanceXu <59581718+haoxiang-xu@users.noreply.github.com>
2023-10-20 16:30:16 -0700

    [1020: ICONs Manager Added]

---
+++ frontend_application/component_lib_testing_application/public/icon_manager.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+import fileTypeIcons512 from "./ICONs/FILETYPE_ICONs/512X512";
+
+import systemIcons512 from "./ICONs/SYSTEM_ICONs/512X512";
+import systemIcons16 from "./ICONs/SYSTEM_ICONs/16X16";
