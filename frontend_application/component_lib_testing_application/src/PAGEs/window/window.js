89a7d913cd1d3676b7bd1b995d29b280567d52ba
skywalker007-cpu <656749873@qq.com>
2023-11-13 14:43:12 -0800

    window submission

--- frontend_application/component_lib_testing_application/src/PAGEs/window/window.js
+++ frontend_application/component_lib_testing_application/src/PAGEs/window/window.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./window.css"
 
-const Window = () => {
-    return <p>This is the window component. </p>;
-};
+function window() {
+    const [messages, setMessages] = useState([
+        { from: 'AI', text: 'What can I help you with today?' },
+        // ... initial messages if any
+    ]);
+    const [inputValue, setInputValue] = useState('');
 
-export default Window;
+    const handleSend = (event) => {
+        event.preventDefault();
+        if (inputValue.length !== 0) {
+            const newMessage = { from: 'User', text: inputValue };
+            setMessages([...messages, newMessage]);
+            setInputValue('');
+            // Scroll to the bottom of the chat history
+        }
+    };
+
+    return (
+        <div className="dialogue-window">
+            <header className="dialogue-header">
+                Currently Referencing: <span className='highlight'>CodeMirror.js</span>, main.py
+            </header>
+            <section className="dialogue-history">
+                {messages.map((message, index) => (
+                    <div key={index} className={`message ${message.from}`}>
+                        {message.text}
+                    </div>
+                ))}
+            </section>
+            <form className="dialogue-input" onSubmit={handleSend}>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    placeholder="Type your message here..."
+                />
+                <button type="submit" className='submit'>Send</button>
+            </form>
+        </div>
+    );
+}
+
+export default window;
