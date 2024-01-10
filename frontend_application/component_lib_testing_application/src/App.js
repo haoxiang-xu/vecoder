ac4d336d13e93eab3e64451fc0fb58f14851296d
prussia1891 <prussia1891@gmail.com>
2024-01-10 11:47:57 -0800

    loadingIcon1
    basically achieve the goal of displaying the loading icon

--- frontend_application/component_lib_testing_application/src/App.js
+++ frontend_application/component_lib_testing_application/src/App.js
@@ -4,6 +4,7 @@ import Home from "./PAGEs/home/home";
 import UploadFile from "./PAGEs/upload_file/uploadfile";
 import CodeEditorPage from "./PAGEs/code_editor/code_editor";
 import DevelopmentEditor from "./PAGEs/development_editor/development_editor";
+import LoadingIcon from "./COMPONENTs/loadingIcon/loadingIcon";
 
 import "./App.css";
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/upload" element={<UploadFile />} />
         <Route path="/codeeditor" element={<CodeEditorPage />} />
         <Route path="/explorer" element={<Home />} />
+        <Route path="/loadingicon" element={<LoadingIcon />} />
       </Routes>
     </Router>
   );
