0052d4060564f95e4a7f523699962481765875e8
Haoxiang Xu <59581718+haoxiang-xu@users.noreply.github.com>
2024-03-15 23:31:48 +0800

    [0315: Styling Changes]

--- frontend_application/component_lib_testing_application/src/App.js
+++ frontend_application/component_lib_testing_application/src/App.js
@@ -4,8 +4,8 @@ import Home from "./PAGEs/home/home";
 import UploadFile from "./PAGEs/upload_file/uploadfile";
 import CodeEditorPage from "./PAGEs/code_editor/code_editor";
 import DevelopmentEditor from "./PAGEs/development_editor/development_editor";
-import VecoderEditorPage from "./PAGEs/vecoder_editor_page/vecoder_editor_page";
 import ChatBox from "./COMPONENTs/chatBox/chatBox";
+import Main from "./PAGEs/main/main";
 
 import "./App.css";
 
@@ -17,7 +17,7 @@ function App() {
         <Route path="/upload" element={<UploadFile />} />
         <Route path="/explorer" element={<Home />} />
         <Route path="/chat" element={<ChatBox />} />
-        <Route path="/" element={<VecoderEditorPage />} />
+        <Route path="/" element={<Main />} />
       </Routes>
     </Router>
   );
---
+++ frontend_application/component_lib_testing_application/src/PAGEs/main/main.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef, useContext } from "react";
+import VecoderEditorPage from "../vecoder_editor_page/vecoder_editor_page";
+import "./main.css";
+
+const Main = () => {
+  return (
+    <div className="main_container0315">
+      <VecoderEditorPage />
+    </div>
+  );
+};
+
+export default Main;
